7fb35c1dc7a0ffa57af08693770f0a080870024c
Alejandro <lbeltran598@unab.edu.co>
2024-06-21 17:47:36 -0500

    login

--- app/src/main/java/com/example/logingoogle/MainActivity.java
+++ app/src/main/java/com/example/logingoogle/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         mTextViewRespuesta = findViewById(R.id.textViewRespuesta);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id)) //no funciona
                 .requestEmail()
                 .build();
 
